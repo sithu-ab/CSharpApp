bbc2b606fec365058d5605f85aaea6f3fe30f239
Sithu <sithu@aluto-benli.com>
2022-02-10 15:42:57 +0630

    Create DataFrame from DataTable using Pandas.NET library

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Data;
 using System.Globalization;
 using ChoETL;
+using Deedle;
+using pd = PandasNet;
 
 namespace CSharpApp
 {
@@ -10,22 +12,29 @@ namespace CSharpApp
         {
             Console.WriteLine("Hello C#!");
 
+            var input_file = "./input/感+プ_07月_千葉県_異常除去済_2_534040_233.csv"; // 入力ファイル
+
+            // ReadCsvIntoDeedleDataFrame(input_file);
+
             // 1.
-            DataTable df = ReadCSV();
+            DataTable tbl = ReadCSV(input_file);
+            pd.DataFrame df = CreateDataFrameFromDataTable(tbl);
+            Console.WriteLine(df);
+
             // 2.
-            GroupBy(df);
+            // GroupBy(df);
         }
 
-
-        static DataTable ReadCSV()
+        /**
+         * Read CSV content using ChoELT library
+         */
+        static DataTable ReadCSV(string input_file)
         {
             Console.WriteLine("-----");
-
-            var input_file = "./input/感+プ_07月_千葉県_異常除去済_2_534040_233.csv"; // 入力ファイル
             Console.WriteLine(input_file);
 
             var p = new ChoCSVReader(input_file).WithFirstLineHeader();
-            var df = p.AsDataTable();
+            var table = p.AsDataTable();
 
             // Console.WriteLine(df.Columns[0]);
 
@@ -38,16 +47,36 @@ namespace CSharpApp
             //     }
             // }
 
-            return df;
+            return table;
         }
 
-        static IDictionary<string, List<object>> GroupBy(DataTable df)
+        /**
+         * Create Pandas DataFrame from DataTable
+         */
+        static pd.DataFrame CreateDataFrameFromDataTable(DataTable tbl)
+        {
+            var rows = new List<PandasNet.Series>();
+
+            foreach(DataRow row in tbl.Rows) {
+                rows.Add(new pd.Series(row.ItemArray.Select(p => p.ToString()).ToArray()));
+            }
+
+            return new pd.DataFrame(rows);
+        }
+
+        /**
+         * Group by data from DataTable using IDictionary
+         */
+        static IDictionary<string, List<object>> GroupBy(DataTable tbl)
         {
             Console.WriteLine("-----");
 
             IDictionary<string, List<object>> dict = new Dictionary<string, List<object>>();
 
-            foreach(DataRow row in df.Rows) {
+
+            foreach(DataRow row in tbl.Rows) {
+                var s = new pd.Series(row.ItemArray);
+
                 // [データの前処理]ID01旅行時間の”8191”をnullに置換(該当行は消さない)
                 // df.loc[df["旅行時間情報"] == 8191, "旅行時間情報"] = np.nan
                 // 旅行時間情報 = row[4]
@@ -79,6 +108,18 @@ namespace CSharpApp
             }
 
             return dict;
+        }
+
+        /**
+         * Read CSV into DataFrame using Deedle library
+         */
+        static void ReadCsvIntoDeedleDataFrame(string input_file)
+        {
+            var df = Frame.ReadCsv(input_file);
+            Console.WriteLine(df);
+
+            Console.WriteLine(df.ColumnCount);
+            Console.WriteLine(df.Columns);
         }
     }
 }
